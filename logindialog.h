a2903d3732f112fc6589e98381ea3f795ee5c326
帕拉丁 <14765475+pala-din@user.noreply.gitee.com>
2026-01-14 22:02:01 +0800

    开始的工程状态

---
+++ logindialog.h
@@ -0,0 +1,23 @@
+#ifndef LOGINDIALOG_H
+#define LOGINDIALOG_H
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class logindialog;
+}
+QT_END_NAMESPACE
+
+class logindialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    logindialog(QWidget *parent = nullptr);
+    ~logindialog();
+
+private:
+    Ui::logindialog *ui;
+};
+#endif // LOGINDIALOG_H
